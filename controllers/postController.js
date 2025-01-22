d34a64746b1f14040d7cb9354eeb4aac77b8d5ae
ErikaCetera <ceteraerika@gmail.com>
2025-01-22 14:23:26 +0100

    feat: bonus

--- controllers/postController.js
+++ controllers/postController.js
@@ -8,63 +8,85 @@ const connection = require('../data/db');
 
 // Funzione per gestire la rotta GET e filtrare i post in base ai tag
 const index = (req, res) => {
-    const sql ="SELECT * FROM `posts`;";
+    const sql = "SELECT * FROM `posts`;";
     connection.query(sql, (err, result) => {
-       if(err){
-        return res.status(500).json({
-            message: "Errore interno server",
-        })
-       } else{
-        return res.status(200).json({
-            status:"success",
-            data: result,
-        })
-       };
+        if (err) {
+            return res.status(500).json({
+                message: "Errore interno server"
+            })
+        } else {
+            return res.status(200).json({
+                status: "success",
+                data: result
+            })
+        };
     })
-    
+
 };
 
 
 // Funzione per gestire la rotta GET per mostrare uno specifico elenento in base all'id
 const show = (req, res) => {
-    const sql = "SELECT * FROM `posts` WHERE `id`= ?";
     const id = req.params.id;
-    connection.query(sql, [id], (err, posts) =>{
-        if(err){
+    const sql = "SELECT * FROM `posts` WHERE `id`= ?";
+
+    const tagsSql = `SELECT posts.* , tags.label 
+    FROM posts 
+    JOIN post_tag 
+    ON posts.id = post_tag.post_id 
+    JOIN tags 
+    ON tags.id = post_tag.tag_id`;
+
+
+    connection.query(sql, [id], (err, posts) => {
+
+        if (err) {
             return res.status(500).json({
-                message: "Errore interno server",
+                message: "Errore interno server"
             })
-        }if(posts.length === 0){
+        }
+        if (posts.length === 0) {
             return res.status(400).json({
-                message: "Post non trovato",
+                message: "Post non trovato"
             })
-        } else{
-            return res.status(200).json({
-                status:"success",
-                data: posts[0],
-            })
-        };
 
-        
+        } else {
+            connection.query(tagsSql, [id], (err, tags) => {
+                if (err) {
+                    return res.status(500).json({
+                        message: "Errore interno del server"
+                    });
+                }
+
+                const postTags = {
+                    ...posts[0],
+                    tags: tags
+                };
+
+                return res.status(200).json({
+                    status: "success",
+                    data: postTags
+                });
+            });
+        }
     })
-
 };
 
 
 // // Funzione che gestisce la rotta POST che crea un nuovo post e lo aggiunge all'array dei posts
 // const store = (req, res) => {
-    
+
 // };
 
 
 // // Funzione che gestisce la rotta PUT che modifica le preprieta di un oggetto (id escluso)
 // const update = (req, res) => {
-    
+
 // };
 
 
 // const modify = (req, res) => {
-    
+
 // };
 
 
@@ -72,12 +94,12 @@ const show = (req, res) => {
 const destroy = (req, res) => {
     const sql = "DELETE FROM `posts` WHERE `id`= ?";
     const id = req.params.id;
-    connection.query(sql, [id], (err) =>{
-        if(err){
+    connection.query(sql, [id], (err) => {
+        if (err) {
             return res.status(500).json({
-                message: "Errore interno server",
+                message: "Errore interno server"
             })
-        } else{
+        } else {
             return res.sendStatus(204)
         };
     })
